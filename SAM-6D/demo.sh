e7312cbd18638dce73c6476ef9d860a5934534d4
AlanKhang_playground <khanggialuu2811@gmail.com>
2025-06-06 23:44:28 +0700

    add dockerfile

--- SAM-6D/demo.sh
+++ SAM-6D/demo.sh
@@ -1,12 +1,16 @@
-# Render CAD templates
-cd Render
-blenderproc run render_custom_templates.py --output_dir $OUTPUT_DIR --cad_path $CAD_PATH #--colorize True 
+export CAD_PATH=Data/Example/obj_000005.ply
+export RGB_PATH=Data/Example/rgb.png
+export DEPTH_PATH=Data/Example/depth.png
+export CAMERA_PATH=Data/Example/camera.json
+export OUTPUT_DIR=Data/Example/outputs
 
+# Render CAD templates
+blenderproc run Render/render_custom_templates.py --output_dir $OUTPUT_DIR --cad_path $CAD_PATH #--colorize True 
 
 # Run instance segmentation model
 export SEGMENTOR_MODEL=sam
 
-cd ../Instance_Segmentation_Model
+cd ./Instance_Segmentation_Model
 python run_inference_custom.py --segmentor_model $SEGMENTOR_MODEL --output_dir $OUTPUT_DIR --cad_path $CAD_PATH --rgb_path $RGB_PATH --depth_path $DEPTH_PATH --cam_path $CAMERA_PATH
 
 
